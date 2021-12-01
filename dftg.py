cd48039e68f323c257288296862b052ea7a30992
DANIEL-SNIECINSKI <daniel.sniecinski@wanadoo.fr>
2021-12-01 09:49:52 +0100

    update

--- dftg.py
+++ dftg.py
@@ -7,7 +7,7 @@ import unittest
 from selenium import webdriver
 #from selenium.webdriver.chrome.options import Options
 #chrome_options = Options()
-#hrome_options.add_argument("--headless")
+#chrome_options.add_argument("--headless")
 #chrome_options.add_argument('--no-sandbox')
 #driver = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
 
