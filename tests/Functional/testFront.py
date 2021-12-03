63a81268eb8e34363d0eae6f7ff5ff38a42cc7a5
DANIEL-SNIECINSKI <daniel.sniecinski@wanadoo.fr>
2021-12-03 16:04:43 +0100

    update

--- tests/Functional/testFront.py
+++ tests/Functional/testFront.py
@@ -10,11 +10,11 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
-#chrome_options.add_argument("--headless")
-#chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--remote-debugging-port9222")
-#chrome_options.add_argument('--no-sandbox')
-#chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument("--disable-dev-shm-usage")
 
 ###########################################################
 
