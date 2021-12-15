89a528cdf6ccfc233960bf2eb6db9b9bc6467ca7
DANIEL-SNIECINSKI <daniel.sniecinski@wanadoo.fr>
2021-12-15 11:33:49 +0100

    update Jenkinsfile

--- tests/Functional/testFront.py
+++ tests/Functional/testFront.py
@@ -1,4 +1,4 @@
-# Ajout ligne3 le 30-11-21
+# Pour exportation des résultats des tests vers des fichiers XML 
 
 import xmlrunner
 
@@ -10,19 +10,14 @@ import unittest
 
 from selenium import webdriver
 
-# Tests du 031221 #####################################################
+# Définition des options néccessaire à l'éxécution des tests dans Jenkins
 
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
-#chrome_options.add_argument("--remote-debugging-port9222")
-#chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.binary_location = '/usr/bin/google-chrome'
-#driver = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
-
-########################################################################
 
 # La classe Keys fournit des touches dans le clavier telles que RETURN, F1, ALT, etc.
 
@@ -31,47 +26,40 @@ from selenium.webdriver.common.keys import Keys
 # La classe de cas de test est héritée de unittest.TestCase
 # Hériter de la classe TestCase est le moyen de dire au module unittest qu'il s'agit d'un cas de test
 
-#class PythonOrgSearch(unittest.TestCase):
 class admin_is_ready(unittest.TestCase):
 
-# Le setUp fait partie de l'initialisation, cette méthode sera appelée avant chaque fonction de test que vous allez écrire dans cette classe de cas de test.
-# Ici, vous créez l'instance de Firefox WebDriver
+# Le setUp fait partie de l'initialisation, cette méthode sera appelée avant chaque fonction de test écrite dans cette classe de cas de test
+# Ici, on crée l'instance de Chrome WebDriver
 
 
     def setUp(self):
-#        self.driver = webdriver.Firefox()
-#        self.driver = webdriver.Chrome()
         self.driver = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
 
-# C'est la méthode des cas de test. La méthode des cas de test doit toujours commencer par les caractères test .
-# La première ligne à l'intérieur de cette méthode crée une référence locale à l'objet pilote créé dans la méthode setUp .
+# C'est la méthode des cas de test. La méthode des cas de test doit toujours commencer par les caractères test 
+# La première ligne à l'intérieur de cette méthode crée une référence locale à l'objet pilote créé dans la méthode setUp 
 
-#    def test_search_in_python_org(self):
-    def test_results(self):
+    def test_dftg_results(self):
         driver = self.driver
 
-# La méthode driver.get naviguera vers une page donnée par l'URL.
+# La méthode driver.get naviguera vers une page donnée par l'URL
 # WebDriver attendra que la page soit complètement chargée
-# (c'est-à-dire que l'événement "onload" se soit déclenché) avant de rendre le contrôle à votre test ou script.
-# Sachez que si votre page utilise beaucoup d'AJAX au chargement,
-# WebDriver peut ne pas savoir quand elle est complètement chargée :
+# (c'est-à-dire que l'événement "onload" se soit déclenché) avant de rendre le contrôle à votre test ou script
 
         driver.get("http://127.0.0.1/prestashop/admin338j8tqkk")
 
-# La ligne suivante est une assertion pour confirmer que le titre contient le mot "dftg":
+# La ligne suivante est une assertion pour confirmer que le titre contient le mot "dftg"
 
         self.assertIn("dftg", driver.title)
 
-# Après la soumission de la page, vous devriez obtenir le résultat selon la recherche s'il y en a.
-# Pour vous assurer que certains résultats sont trouvés, faites une assertion :
+# Après la soumission de la page, on doit obtenir le résultat selon la recherche s'il y en a
+# Pour s'assurer que certains résultats sont trouvés, on fait une assertion 
 
         assert "No results found." not in driver.page_source
 
-# La méthode tearDown sera appelée après chaque méthode de test.
-# C'est un endroit pour faire toutes les actions de nettoyage. Dans la méthode actuelle, la fenêtre du navigateur est fermée.
-# Vous pouvez également appeler la méthode quit au lieu de close .
+# La méthode tearDown sera appelée après chaque méthode de test
+# C'est un endroit pour faire toutes les actions de nettoyage. Dans la méthode actuelle, la fenêtre du navigateur est fermée# Vous pouvez également appeler la méthode quit au lieu de close .
 # Le quit va quitter le navigateur entier, alors à proximité se fermer un onglet, mais si elle est la seule onglet ouvert,
-# par défaut le plus navigateur va quitter entièrement .:
+# par défaut le plus navigateur va quitter entièrement
 
     def tearDown(self):
         self.driver.close()
